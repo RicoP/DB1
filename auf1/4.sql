6003c18deaf5514e32b3548efa651383e704d90d
Rico P Mac <mailtorico@googlemail.com>
2011-11-02 08:51:53 +0100

    4 verbessert

--- auf1/4.sql
+++ auf1/4.sql
@@ -1,9 +1,13 @@
-SELECT kunde.firma, T.maxpreis FROM (SELECT kopf.kdnr as kdnr, SUM(position.menge * position.preis) AS maxpreis
-from aufpos AS position 
-JOIN aufkopf as kopf 
-WHERE  
-kopf.aufnr = position.aufnr 
-group  by kopf.kdnr) AS T  
-JOIN kdst AS kunde 
-where kunde.kdnr = T.kdnr
-ORDER BY maxpreis  DESC 
+T kunde.firma, T.maxpreis
+FROM (
+
+SELECT kopf.kdnr AS kdnr, SUM( position.menge * position.preis ) AS maxpreis
+FROM aufpos AS position
+JOIN aufkopf AS kopf
+WHERE kopf.aufnr = position.aufnr
+GROUP BY kopf.kdnr
+) AS T
+JOIN kdst AS kunde
+WHERE kunde.kdnr = T.kdnr
+ORDER BY maxpreis DESC 
+LIMIT 0 , 1
